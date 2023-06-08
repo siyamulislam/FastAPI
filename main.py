2fb0a9da1a1ae337928046abb5761a497069e0bc
siyamulislam <siyamulislam1@gmail.com>
2023-06-09 02:00:17 +0600

    feedback of fast api

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 from fastapi import FastAPI
+from enum import Enum
+app = FastAPI()
 
-app =FastAPI()
-
-@app.get("/hello")
-async def hello():
-    return {'hello':'world'}
-@app.get("/hello/{name}")
-async def hello_person(name:str):
-    return {'hello':name}
 
 @app.get("/")
 async def hello():
-    return {'hello':'home'}
+    return {'hello': 'home'}
+
+
+@app.get("/hello/{name}")
+async def hello_person(name: str):
+    return {'hello': name}
+
+class AvailableCuisines (str, Enum) :
+    indian = "indian"
+    american = "american"
+    italian = "italian"
+
+food_items = {
+    'indian': ["Samosa", "Dosa"],
+    'american': ["Hot Dog", "Apple pie"],
+    'italian': ["Ravioli", "pizza"]
+}
+valid_cuisines = food_items.keys()
+
+# @app.get("/get_items/{cuisine}")
+# async def get_items(cuisine):
+
+#     if cuisine not in valid_cuisines:
+#         return f"Supported cuisines are {valid_cuisines}"
+
+#     return food_items. get(cuisine)
+
+
+@app.get("/get_items/{cuisine}")
+async def get_items(cuisine:AvailableCuisines):
+
+    return food_items. get(cuisine)
+
+coupon_code = {
+    1: '10%',
+    2: '20%',
+    3: '30%'
+}
+
+@app.get("/get_coupon/{code}")
+async def get_items(code: int):
+    return {'discount_amount': coupon_code.get(code) }
